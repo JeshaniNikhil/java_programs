eb8e5e9505a54621e0760a17b58c7bcf4a8b31d7
JeshaniNikhil <140085318+JeshaniNikhil@users.noreply.github.com>
2023-12-31 12:59:27 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Arraylist {
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> l1=new ArrayList<>();
+        l1.add(10);
+        l1.add(20);
+        //l1.remove(0);
+        System.out.println(l1. size());
+        System.out.println(l1);
+    }
+    
+}
